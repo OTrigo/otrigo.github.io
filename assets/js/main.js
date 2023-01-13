e6ff1e45ba20d9dc7ce62adb5ecbaa1fd33af6b4
Jorge Lucas <37757146+thepinkwheat@users.noreply.github.com>
2023-01-13 20:22:54 -0300

    portfólio 2.0
    Atualização no meu portfólio, agora utilizando bootstrap e sass. O próximo será feito com React.

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("scroll", function(){
-  const header = document.getElementById("nav")
-  const header_content = document.getElementById("nav-content")
-  header.classList.toggle("sticky", window.scrollY > 0)
-  header_content.classList.toggle("sticky-content", window.scrollY > 0)
-})
--- projetos/mini_projetos/desafio_codificador_ONE/js/functions/reset.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("reset").addEventListener("click", function () {
-
-    let reset = document.getElementById("final-text").value = " ";
-    document.getElementById("msg").value = " ";
-
-    alert("o texto foi Limpo: " + reset);
-})
